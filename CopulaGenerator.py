1598a8e0c4894c415ce205be2b8d1ffe390af3c2
Biko Pougala <bpougala@gmail.com>
2019-11-27 16:36:29 +0000

    Proper implementation of all 3 copulas in the multidimensional case

--- CopulaGenerator.py
+++ CopulaGenerator.py
@@ -37,98 +37,54 @@ class CopulaGenerator:
     def  MultiDimensionalClayton(self, alpha, d=(2,1000)):
       self.alpha = alpha 
       arr = []
+      v = gamma.rvs(a=1 / alpha, scale=1, size=(d[1],))
       for i in range(d[0]):
         x = np.random.uniform(size=(d[1],))
-        v = gamma.rvs(a=1/alpha, scale=1, size=(d[1],))
         u_x = self.clayton_generator(-np.log10(x) / v)
-        print("Size of u_x: " + str(u_x.shape))
         arr.append(u_x)
-        print("Size of arr: " + str(len(arr))) 
       
-      return arr  
-    def Clayton(self, alpha, d=(1000,)):
+      return np.asarray(arr)  
+    def Clayton(self, x, alpha, d=(1000,)):
+        s = np.random.RandomState(1234) 
         self.alpha = alpha
-        x = np.random.uniform(size=d)
-        y = np.random.uniform(size=d)
-        v = gamma.rvs(a=1 / alpha, scale=1, size=d)
+        #x = np.random.uniform(size=d)
+        y = s.uniform(size=d)
+        v = gamma.rvs(a=1 / alpha, scale=1, size=d, random_state=s)
         u_x = self.clayton_generator(-np.log10(x) / v)
         u_y = self.clayton_generator(-np.log10(y) / v)
-        return u_x, u_y
+        return v
 
     def gumbel_generator(self, t):
         return np.exp(-t ** (1 / self.alpha))
 
+    def MultiDimensionalGumbel(self, alpha, d=(2,1000)):
+        self.alpha = alpha 
+        arr = []
+        beta = (math.cos(math.pi / (2 * alpha))) ** alpha
+        v = levy_stable.rvs(1 / alpha, 1, loc=0, scale=beta, size=(d[1],))
+        for i in range(d[0]):
+          x = np.random.uniform(size=(d[1],))
+          u_x = self.gumbel_generator(-np.log10(x) / v)
+          arr.append(u_x)
+
+        return np.asarray(arr)
     def Gumbel(self, alpha, d=(1000,)):
         self.alpha = alpha
         x = np.random.uniform(size=d)
         y = np.random.uniform(size=d)
         beta = (math.cos(math.pi / (2 * alpha))) ** alpha
-        v = levy_stable.rvs(1 / alpha, 1, scale=1, size=d)
+        v = levy_stable.rvs(1 / alpha, 1, loc=0, scale=beta, size=d)
         u_x = self.gumbel_generator(-np.log10(x) / v)
         u_y = self.gumbel_generator(-np.log10(y) / v)
         return u_x, u_y
 
-    def frank_psi(self, u1, v2):
-        numerator = np.expm1(self.alpha) * v2
-        denominator = v2 * np.expm1(-self.alpha * u1) - np.exp(self.alpha * u1)
-        u2 = -1 / self.alpha * np.log1p(numerator / denominator)
-        return u2
-
-    def frank2d(self, alpha, d=(1000,)):
-        self.alpha = alpha
-        v1 = np.random.uniform(size=d)
-        v2 = np.random.uniform(size=d)
-        U_1 = v1
-        U_2 = self.frank_psi(U_1, v2)
-
-        return U_1, U_2
-
-    def hello(self):
-        return "hello"
-
     def generate_data(self, copulas, lambdas=None):
-        print("calling removeNans")
+        arr = []
         if lambdas is None:  # if no vars is passed, randomly generate dependence
             lambdas = np.random.uniform(0.5, 6, size=len(copulas))
             firstArr = self.removeNans(copulas[0])
         arr_poisson = np.array(poisson.ppf(firstArr, lambdas[0]))
-        for i in range(1, len(copulas)):
+        for i in range(len(copulas)):
             poiss = np.array(poisson.ppf(self.removeNans(copulas[i]), lambdas[i]))
-            np.concatenate((arr_poisson, poiss), axis=0)
-        return arr_poisson
-
-    def inverse_transform_frank(self, k):
-        return ((1 - math.exp(-k)) ** k) / k * self.alpha
-
-    def RLAPTRANS_implementation(self, n, b, k_max):
-        u = np.sort(np.random.uniform(size=(n,)))
-        # Find a value x_max such that F(x_max)
-        x_start = 1
-        j_max = 1
-        x_max = x_start
-        j = 0
-        while self.inverse_transform_frank(x_max) < u[-1] and j < j_max:
-            x_max = b * x_max
-            j += 1
-        if j == j_max:
-            return 1
-        x = list()
-        x.append(0)
-        for i in range(1, n + 1):
-            x_L = x[i - 1]
-            x_U = x_max
-            k = 0
-            t = x_L
-            while (np.linalg.norm(self.inverse_transform_frank(t)) > t and k < k_max):
-                k += 1
-                t = t - (self.inverse_transform_frank(t) - u[i]) / self.frank_transform(t)
-                if t not in range(x_L, x_U):
-                    x_L = t
-                else:
-                    x_U = t
-            if k == k_max:
-                return 1
-            else:
-                x[i] = t
-
-        return np.random.permutation(x)
+            arr.append(poiss)
+        return np.asarray(arr)
