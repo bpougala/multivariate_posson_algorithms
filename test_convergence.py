eb94a2750cb0d29d120fca35398e703ca5d4f192
Biko Pougala <biko.pougala@outlook.com>
2020-03-31 22:35:16 +0200

    Gaussian copula convergence testings working

--- test_convergence.py
+++ test_convergence.py
@@ -1,6 +1,8 @@
+import sys
 from functools import reduce
 
 import numpy as np
+from scipy.stats import random_correlation
 
 import MultivariatePoisson as mvp
 from MultivariatePoisson import MultivariatePoisson as mvp
@@ -12,12 +14,30 @@ def Average(lst):
     return reduce(lambda a, b: a + b, lst) / len(lst)
 
 
-def generate_experiment_gaussian(data_size, data_dimensions, cov=None):
-    if cov is None:
-        cov = np.array([[1, 0.3], [0.3, 1]])
-    mean = np.array([2, 7])
+def generate_experiment_gaussian(num_dim, num_samples):
+    cov = None
+    mean = None
+    cov_2 = np.array([[1, 0.2], [0.2, 1]])
+    cov_3 = random_correlation.rvs((1.1, 0.7, 1.2))
+    cov_4 = random_correlation.rvs((0.3, 1.6, 0.8, 1.3))
+    cov_5 = random_correlation.rvs((0.3, 1.8, 1.2, 1.1, 0.6))
+    cov_6 = random_correlation.rvs((.8, 2.0, .6, 1.5, .1, 1.0))
+    if num_dim == 2:
+        cov = cov_2
+    elif num_dim == 3:
+        cov = cov_3
+    elif num_dim == 4:
+        cov = cov_4
+    elif num_dim == 5:
+        cov = cov_5
+    elif num_dim == 6:
+        cov = cov_6
+    else:
+        raise Exception("Invalid number of dimensions chosen")
+    mean = np.random.randint(0, 20, size=num_dim)
     multipoiss = mvp(cov=cov, family="gaussian")
-    data = multipoiss.rvs(mu=mean, size=(data_dimensions, data_size))
+    data = multipoiss.rvs(mu=mean, size=(cov.shape[0], num_samples))
+    print("data: " + str(data))
     pmf = multipoiss.pmf(data, mean)
     cov_hat, mean_hat = multipoiss.optimise_params(data=data)
     multipoiss_hat = mvp(cov=cov_hat, family="gaussian")
@@ -30,7 +50,7 @@ def kullback_leibler(pmf_x, pmf_y):
 
 
 def kl_divergence(p, q):
-    return np.sum(np.where(p != 0, p * np.log10(p / q), 0))
+    return np.sum(np.where(p != 0, p * np.log(p / q), 0))
 
 
 def generate_experiment(data_size, data_dimensions, family, alpha=None, cov=None):
@@ -42,7 +62,9 @@ def generate_experiment(data_size, data_dimensions, family, alpha=None, cov=None
 
 
 def main():
-    kl = generate_experiment_gaussian(20, 2)
+    num_dimensions = int(sys.argv[1])
+    num_samples = int(sys.argv[2])
+    kl = generate_experiment_gaussian(num_dimensions, num_samples)
     print("Kullback-Leibler value is now: " + str(kl))
 
 
